23e60647fda29ba455f371e2104be55479258e84
Danny Hyeong Yu <hyeong.yu@mail.utoronto.ca>
2019-07-30 23:51:07 -0400

    new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import AutoCompleteText from './AutoCompleteText';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-Component">
+            <AutoCompleteText />
+        </div>      
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/AutoCompleteText.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './AutoCompleteText.css';
+
+export default class AutoCompleteText extends React.Component {
+    constructor(props){
+        super(props);
+        this.items = [];
+        this.state = {
+            suggestions: [],
+            text: '',
+            data: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    data: json,
+                })
+        });
+        
+    }
+
+    onTextChanged = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0){
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => regex.test(v));
+        } 
+        this.setState(() => ({suggestions, text: value}));
+    }
+
+    renderSuggestions () {
+        const { suggestions, data } = this.state;
+        console.log(data)
+        this.items = this.state.data.map(item => item.name);
+
+        if (suggestions.length === 0){
+            return null;
+        } 
+        return (
+            <ul>
+                {suggestions.map((item) => <li onClick={() => this.suggestionSelected(item)}>{item}</li>)}
+            </ul>
+        );
+    }
+
+    suggestionSelected (value) {
+        this.setState(() => ({
+            text: value,
+            suggestions: [],
+        }))
+    }
+
+    render () {
+        const { text, isLoaded } = this.state;
+
+
+        if(!isLoaded){
+            return <div>...Loading</div>
+        }
+
+        return (
+            <div className="AutoCompleteText">
+                <input value={text} onChange={this.onTextChanged} type="text" />
+                {this.renderSuggestions()}
+            </div>
+        )
+    }
+}
